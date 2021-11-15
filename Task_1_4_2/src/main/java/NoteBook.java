690bf6b2e49c87f7d38f2f7ff9def99489d92b4c
Eres <e.kuular@g.nsu.ru>
2021-11-15 17:11:13 +0700

    task 1 4 2

--- Task_1_4_2/src/main/java/NoteBook.java
+++ Task_1_4_2/src/main/java/NoteBook.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class NoteBook {
-    ArrayList<Note> notes;
+    private ArrayList<Note> notes;
 
     NoteBook() {
         notes = new ArrayList<>();
