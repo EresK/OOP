222ba0592bd573704732d4445adf026241571aaa
EresK <e.kuular@g.nsu.ru>
2022-02-26 21:34:59 +0700

    add linear solving

---
+++ Task_2_1/src/main/java/PrimeNumber.java
@@ -0,0 +1,36 @@
+public class PrimeNumber {
+    public boolean isAllPrime(long[] array) {
+        boolean isPrimeFlag = true;
+
+        for (long num: array) {
+            if (!isPrime(num)) {
+                isPrimeFlag = false;
+                break;
+            }
+        }
+
+        return isPrimeFlag;
+    }
+
+    public boolean isPrime(long number) {
+        double numSqrt = Math.sqrt((double) number);
+        long numBound = Math.round(numSqrt);
+
+        boolean isPrimeFlag = true;
+
+        if (number % 2 == 0) {
+            if (number != 2)
+                isPrimeFlag = false;
+        }
+        else {
+            for (long i = 3; i <= numBound; i += 2) {
+                if (number % i == 0) {
+                    isPrimeFlag = false;
+                    break;
+                }
+            }
+        }
+
+        return isPrimeFlag;
+    }
+}
