7d9bfb84e8f6be1ed043a2133fa484c605347870
EresK <e.kuular@g.nsu.ru>
2022-06-07 12:28:16 +0700

    -

--- Pizzeria/src/main/java/Program.java
+++ Pizzeria/src/main/java/Program.java
@@ -1,37 +1,31 @@
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
 import java.util.ArrayList;
 
 public class Program {
     public static void main(String[] args) {
-        String pathMakers = "./src/main/resources/makers";
-        String pathDeliverers = "./src/main/resources/deliverers";
-        String pathQueue = "./src/main/resources/queue";
+        String pathSerialized = "./src/main/resources/serialized";
 
         try {
-            runPizzeria(pathMakers, pathDeliverers, pathQueue);
+            ArrayList<Integer> exp = new ArrayList<>();
+            exp.add(1);
+            exp.add(2);
+            exp.add(3);
+            ArrayList<Integer> back = new ArrayList<>();
+            back.add(5);
+            back.add(5);
+            back.add(5);
+
+            Pizzeria pizzeria = new Pizzeria(15, 10, 1, exp, back);
+
+            /*PizzeriaSerializer s = new PizzeriaSerializer();
+            s.serialize(pathSerialized, pizzeria);*/
+
+            /*PizzeriaDeserializer d = new PizzeriaDeserializer();
+            pizzeria = d.deserialize(pathSerialized);*/
+
+            // pizzeria.run();
         }
         catch (Exception ex) {
             System.err.println(ex.getMessage());
-        }
-    }
-
-    public static void runPizzeria(String pathMakers, String pathDeliverers, String pathQueue) throws Exception {
-        File fileMakers = new File(pathMakers);
-        File fileDeliverers = new File(pathDeliverers);
-        File fileQueue = new File(pathQueue);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        ArrayList<Integer> makers = objectMapper.readValue(fileMakers, new TypeReference<>(){});
-        ArrayList<Integer> deliverers = objectMapper.readValue(fileDeliverers, new TypeReference<>() {});
-        ArrayList<Integer> queues = objectMapper.readValue(fileQueue, new TypeReference<>() {});
-
-        if (queues.size() == 3) {
-            Pizzeria pizzeria = new Pizzeria(queues.get(0), queues.get(1), queues.get(2), makers, deliverers);
-            pizzeria.run();
         }
     }
 }
